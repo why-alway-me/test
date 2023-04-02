97705b81962aeef549191c201129b17e2be84cd0
why-alway-me <104372788+why-alway-me@users.noreply.github.com>
2023-04-02 10:19:04 +0700

    update

--- login.php
+++ login.php
@@ -1,8 +1,7 @@
 <?php
 // Thiết lập thông tin kết nối đến database
 
-$servername = "database-server-lab7.cocgl5wbv5ga.ap-southeast-
-1.rds.amazonaws.com";
+$servername = "database-server-lab7.c1pnds6s3k1f.ap-southeast-1.rds.amazonaws.com";
 
 $username = "admin";
 $password = "12345678";
